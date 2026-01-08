fac70ac31f5a7b45b87aea3e27b2c8337f3ad389
Aleyna <104082194+aleynakaynak@users.noreply.github.com>
2026-01-08 13:57:07 +0300

    project setup + routing + components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Success from "./components/Success";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
